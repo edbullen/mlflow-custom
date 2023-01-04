e755fa2474dbab66c89de77d9dcc89b84370fd26
Ed Bullen <ed.bullen@gmail.com>
2023-01-04 19:57:46 +0000

    initial commit

---
+++ functions/HybridFunction.py
@@ -0,0 +1,49 @@
+import mlflow
+import pandas as pd
+
+
+class HybridFunction(mlflow.pyfunc.PythonModel):
+    """ This Class wraps a custom function in MLflow, so that it can be parameterised, tracked and deployed with MLflow
+
+    Attributes:
+        x0: boundary on x-axis between function A and function B
+        y0: y-axis value for function A output and base-value for function B
+        gradient: slope / gradient for increase in y given x (result) for function B
+
+    """
+
+    def __init__(self, x0, y0, gradient):
+        self.x0 = x0
+        self.y0 = y0
+        self.gradient = gradient
+
+    def _custom_function(self, model_input :pd.DataFrame):
+        """ Hybrid function combines two different functions depending on the value of x
+            x < x0 is a constant value y0
+            x >= x0 is uniform gradient function (straight-line graph) y = gradient*x + y0 (y0 is the intercept)
+
+        Args:
+           model_input: :pandas.DataFrame:
+
+        Returns:
+            model_output: :pandas.DataFrame:
+        """
+
+        y_intercept = self.y0 - self.gradient * self.x0
+
+        model_output = model_input.apply(lambda x: self.y0 if x < self.x0 else self.gradient*x + y_intercept)
+
+        return model_output
+
+    def predict(self, context, model_input :pd.DataFrame):
+        """ MLflow entry-point to apply the function to a data payload
+        Args:
+            context: A :class:`~PythonModelContext` instance containing artifacts that the model
+                        can use to perform inference. https://mlflow.org/docs/latest/_modules/mlflow/pyfunc/model.html
+            model_input: :pandas.Dataframe: series of x-values to apply the _custom_function() to
+
+        Returns:
+            model_output: :pandas.Dataframe:
+        """
+
+        return model_input.apply(self._custom_function)
---
+++ tests/test_function.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from functions.HybridFunction import HybridFunction
+
+
+def test_custom_function():
+    """ test function behaves as expected:
+    values less than x0 -> y0, values greater-equal x0 -> y = mx + c   (eqn of straight line)
+        m is the gradient, c is the intercept = y0 - (gradient * x0)
+    """
+    # instance of the model function with x0, y0, gradient set
+    model = HybridFunction(x0=5, y0=2, gradient=1)
+    # test data in a Pandas series -2 ... 12
+    data = pd.DataFrame([range(-2, 13)])
+
+    # apply the model's _custom_function to get output in a Pandas series
+    model_output = data.apply(model._custom_function, axis=1)
+
+    # print(int(data[0]))
+    # print(int(data[7]))
+    # print(int(data[14]))
+
+    assert int(model_output[0]) == 2
+    assert int(model_output[7]) == 2
+    assert int(model_output[12]) == 7
