f4af49e44668aeb14ba60ca97390eb37c4cb529a
edbullen <edbullen>
2023-01-05 11:30:56 +0000

    place-holder Databricks notebooks for deploying and running in MLflow

---
+++ notebooks/hybrid_function_deploy.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Deploy Custom Function to MLflow
+# MAGIC Notebook to deploy an instance of the `HybridFunction` class in MLflow with function attributes set to parameterise the custom function.
---
+++ notebooks/hybrid_function_run.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Run Custom Function using MLflow
+# MAGIC Notebook to call an instance of the `HybridFunction` class that has been deployed in MLflow.  
+# MAGIC Multiple instances of the Hybrid Function can be stored in MLflow with different function parameters.  
+# MAGIC MLflow can be used to choose a particular function configuration and execute it against a data-pipeline.  
